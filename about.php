517549755e2524dd27ac054874ba8501cfb2cd6f
Marceau <marceau.pheulpin@hotmail.fr>
2018-05-15 09:21:59 +0200

    contact et about

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marceaupheulpin
+ * Date: 15/05/2018
+ * Time: 09:20
+ */
---
+++ contact.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+</head>
+
+<body>
+
+<h1>Contact Us</h1>
+
+</body>
+</html>
